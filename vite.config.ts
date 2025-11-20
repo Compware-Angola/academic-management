4cabf705160da4a5d2ff5bf252f20dc1b7312182
Domingos Canhanga <canhanga96@gmail.com>
2025-11-20 12:24:42 +0100

    chore: chore port

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 4100,
+    port: 4200,
     open: true,
   },
   resolve: {
