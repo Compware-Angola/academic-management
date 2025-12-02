c021356e2069471e3c7c304af06095005df5252a
Domingos Canhanga <canhanga96@gmail.com>
2025-12-02 02:09:36 +0100

    feat: validar horario por director

---
+++ src/hooks/horario/use-query-validar-horario-director.ts
@@ -0,0 +1,34 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useToast } from "@/components/ui/use-toast";
+import { ValidarHorarioDirectorService } from "@/services/horario/validar-horario-director.service";
+
+export const useMutationValidarHorarioDirector = () => {
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+
+  return useMutation({
+    mutationFn: ValidarHorarioDirectorService,
+
+
+    onSuccess: () => {
+
+      queryClient.invalidateQueries({
+        queryKey: ["horarios-existentes"],
+      });
+
+      toast({
+        title: "Validado!",
+        description: "O horário foi validado com sucesso.",
+      });
+    },
+    onError: (error: any) => {
+      toast({
+        title: "Erro ao validar horário",
+        description:
+          error?.response?.data?.message ||
+          "Ocorreu um problema ao tentar validar o horário.",
+        variant: "destructive",
+      });
+    },
+  });
+};
---
+++ src/services/horario/validar-horario-director.service.ts
@@ -0,0 +1,19 @@
+import { axiosApexGa } from "@/lib/axios-apex-ga";
+
+
+
+export type ValidarHorarioDirectoPayload = {
+  p_horario_id: number | string;
+};
+
+export async function ValidarHorarioDirectorService(
+  payload: ValidarHorarioDirectoPayload,
+): Promise<void> {
+  const { p_horario_id } = payload;
+
+  await axiosApexGa.put("/horario/Validar", {
+    params: {
+      p_horario_id,
+    },
+  });
+}
