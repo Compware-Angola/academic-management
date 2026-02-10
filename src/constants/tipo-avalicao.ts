3488f2ffe87e2677e2166208cf1b43cccc037fe2
Domingos Canhanga <canhanga96@gmail.com>
2026-02-10 10:14:06 +0100

    feat: Introduce `TIPO_AVALIACAO` constant to map evaluation type names to IDs and update `LaunchNotes` to utilize this mapping for queries.

---
+++ src/constants/tipo-avalicao.ts
@@ -0,0 +1,18 @@
+export const TIPO_AVALIACAO = {
+    "1ª Frequência": 1,
+    "2ª Frequência": 2,
+    "Prática": 3,
+    "Nota da Avaliação Contínua": 4,
+    "Exame": 5,
+    "Recurso": 6,
+    "Prova Oral": 7,
+    "Época Especial": 8,
+    "NFC": 9,
+    "Nota Final Lançada": 10,
+    "Lançar nota da 1ª Frequência": 11,
+    "Prova Oral de Recurso": 12,
+    "Melhoria de Notas": 13,
+    "2ª Frequência/Exame": 14
+}
+
+
