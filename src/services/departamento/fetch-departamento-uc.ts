95a93dbafb410359abbfd0833a8215a2d21d0bf9
nzinga74 <nzingaenoque@gmail.com>
2026-03-12 18:23:44 +0100

    Departamento

--- src/services/departamento/fetch-departamento-uc.ts
+++ src/services/departamento/fetch-departamento-uc.ts
@@ -1,23 +1,50 @@
-import { axiosApexGa } from "@/lib/axios-apex-ga";
-export type DepartamentoUC = {
-  codigo_grade: number;
-  disciplina: string;
-  classe: string;
-  semestre: string;
+import { axiosNestGa } from "@/lib/axios-nest-ga";
+
+export type GetDisciplineDepartmentPayload = {
+  departamento: number;
+  classe: number;
+  semestre: number;
+  page?: number;
+  limit?: number;
 };
-type DepartamentoUCParams = {
-  departamento: string;
+
+export type DisciplineDepartment = {
+  codigo_grade: number;
+  codigo_disciplina: number;
+  unidade_curricular: string;
+  codigo_classe: number;
+  ano_curricular: string;
+  codigo_semestre: number;
   semestre: string;
-  classe: string;
+  codigo_departamento: number;
+  status: number;
+};
+
+export type GetDisciplineDepartmentResponse = {
+  data: DisciplineDepartment[];
+  total: number;
+  page: number;
+  limit: number;
+  totalPages: number;
 };
 
 export async function fetchDepartamentoUC(
-  params: DepartamentoUCParams
-): Promise<DepartamentoUC[]> {
-  const { departamento, semestre, classe } = params;
-  const { data } = await axiosApexGa.get(
-    `ga/curricular-unit/department/${departamento}/${classe}/${semestre}`
+  payload: GetDisciplineDepartmentPayload,
+): Promise<GetDisciplineDepartmentResponse> {
+  const { departamento, classe, semestre, page = 1, limit = 25 } = payload;
+
+  const { data } = await axiosNestGa.get<GetDisciplineDepartmentResponse>(
+    "/discipline/departamento",
+    {
+      params: {
+        departamento,
+        classe,
+        semestre,
+        page,
+        limit,
+      },
+    },
   );
 
-  return data.unidade_curriculares ?? [];
+  return data;
 }
