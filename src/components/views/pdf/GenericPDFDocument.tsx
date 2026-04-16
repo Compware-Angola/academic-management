b97c02c4e94c744afef21ee08c47f24909ee109c
Domingos Canhanga <canhanga96@gmail.com>
2026-04-16 11:14:34 +0100

    refactor: rename orientation prop from portrait to vertical in GenericPDFDocument

--- src/components/views/pdf/GenericPDFDocument.tsx
+++ src/components/views/pdf/GenericPDFDocument.tsx
@@ -167,7 +167,7 @@ export function GenericPDFDocument(props: GenericPDFProps) {
     header = defaultHeader,
     documentTitle,
     subtitle,
-    orientation = "portrait",
+    orientation = "vertical",
     infoSections = [],
     mainTable,
     totals = [],
