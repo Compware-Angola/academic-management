8816bda3897efd86ed18f744a4f4da446f470007
nzinga74 <nzingaenoque@gmail.com>
2026-04-23 12:00:02 +0100

    Resolver o bug de mudança

--- src/pages/estudante/PerfilSection/index.tsx
+++ src/pages/estudante/PerfilSection/index.tsx
@@ -51,7 +51,6 @@ const TABS_CONFIG = [
     component: InscricoesUC,
   },
   {
-
     value: "definir-especialidade",
     label: "Definir Especialidade",
     icon: Book,
@@ -60,12 +59,10 @@ const TABS_CONFIG = [
 
   {
     value: "diplomar",
-  label: "Diplomar",
-  icon: GraduationCap,
-  component: Diplomar,
-  }
-
-
+    label: "Diplomar",
+    icon: GraduationCap,
+    component: Diplomar,
+  },
   {
     value: "mudar-curso",
     label: "Mudança do Curso",
