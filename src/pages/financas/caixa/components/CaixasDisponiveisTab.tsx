01a304d2ab9ff7e4fc91d69db07178814dbb5d93
Domingos Canhanga <canhanga96@gmail.com>
2026-05-25 11:18:11 +0100

    refactor: remove unused buttonVariants import in CaixasDisponiveisTab

--- src/pages/financas/caixa/components/CaixasDisponiveisTab.tsx
+++ src/pages/financas/caixa/components/CaixasDisponiveisTab.tsx
@@ -19,7 +19,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
   Dialog,
