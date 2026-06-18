6fbadde97c54287276e2f0222374f3843d2b62aa
Domingos Canhanga <canhanga96@gmail.com>
2026-06-18 10:43:28 +0100

    feat: modularize application routes and initialize router refactoring structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import App2 from "./App2.tsx";
 
-
-createRoot(document.getElementById("root")!).render(<App />);
+const REFACTOR_ROUTER = "YES_REFACTOR_ROUTER"
+createRoot(document.getElementById("root")!).render(REFACTOR_ROUTER === "YES_REFACTOR_ROUTER" ? <App2 /> : <App />);
