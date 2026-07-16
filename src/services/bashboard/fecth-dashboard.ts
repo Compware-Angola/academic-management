e4295357304ffdaa8f301d95612eda62d97ce62b
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2026-07-16 14:45:56 +0100

    feat: integrate dashboard statistics reports service and update UI to display dynamic student enrollment data

--- src/services/bashboard/fecth-dashboard.ts
+++ src/services/bashboard/fecth-dashboard.ts
@@ -1,11 +1,41 @@
 import { axiosApexGa } from "@/lib/axios-apex-ga";
+import { axiosNestGa } from "@/lib/axios-nest-ga";
 export type Dashboard = {
-"total_estudantes": number,
-"total_docentes": number,
-"total_uc": number,
-"aval_pendentes": number
+  "total_estudantes": number,
+  "total_docentes": number,
+  "total_uc": number,
+  "aval_pendentes": number
 };
 export async function fetchDashboard(): Promise<Dashboard> {
   const { data } = await axiosApexGa.get("/uma/dashboard");
   return data
+}
+
+
+
+
+
+export type DashboardStatisticsReports = {
+  totalAlunosInscritos: {
+    total: number,
+    descricao: string
+  }
+  totalDocentes: {
+    total: number,
+    descricao: string
+  }
+  totalUCs: {
+    total: number,
+    descricao: string
+  }
+  avaliacoesPendentes: {
+    total: number,
+    descricao: string
+  }
+};
+
+export async function fetchDashboardStatisticsReports(): Promise<DashboardStatisticsReports> {
+  const { data } = await axiosNestGa.get("/statistics-reports/dashboard");
+
+  return data;
 }
