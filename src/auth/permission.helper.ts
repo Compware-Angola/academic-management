d9ff3a431adfb1306eba0d53f21695358a897a0b
Isaac Bunga <isaacisvaldobunga300@gmail.com>
2026-08-11 16:23:51 +0100

    refactor: update hasPermission to support options and optimize haveFullAccess check

--- src/auth/permission.helper.ts
+++ src/auth/permission.helper.ts
@@ -1,23 +1,29 @@
-// use-permission.ts
-
 import { useCurrentUser } from "@/hooks/mutations/use-mutation-login";
 import { can } from "./can";
 import { PermissionTypeDetails } from "@/constants/permission.type";
-
 
 export function usePermission() {
   const { data: user } = useCurrentUser();
 
   const userPermissions: string[] = user?.permissions || [];
 
-  const hasPermission = (required?: string | string[]) => {
-    const hasPer = can(userPermissions, required)
-    return hasPer
+  const hasPermission = (
+    required?: string | string[],
+    options?: {
+      blockFullAccess?: boolean;
+    }
+  ) => {
+    return can(userPermissions, required, options);
   };
-  const haveFullAccess = () => {
-    const hasPer = can(userPermissions, PermissionTypeDetails.FULL_ACCESS.sigla)
-    return hasPer
-  }
 
-  return { hasPermission, haveFullAccess };
+  const haveFullAccess = () => {
+    return userPermissions.includes(
+      PermissionTypeDetails.FULL_ACCESS.sigla
+    );
+  };
+
+  return {
+    hasPermission,
+    haveFullAccess,
+  };
 }
